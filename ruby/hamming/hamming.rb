e94ce527ac1c870ec69f65213a8fb8b19320ea7b
Abhishek Srivastava <abhishek77in@gmail.com>
2016-01-11 16:45:28 +0530

    Refactor hamming solution

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -3,11 +3,18 @@ class Hamming
 
   def self.compute(strand_one, strand_two)
     validate_inputs(strand_one, strand_two)
+    hamming_distance(strand_one, strand_two)
+  end
 
+  def self.hamming_distance(strand_one, strand_two)
     strand_length = strand_one.length
     strand_length.times.inject(0) do |sum, index|
-      sum + (strand_one[index] <=> strand_two[index]).abs
+      sum + hamming_diff(strand_one[index], strand_two[index])
     end
+  end
+
+  def self.hamming_diff(nucleotide_one, nucleotide_two)
+    (nucleotide_one <=> nucleotide_two).abs
   end
 
   def self.validate_inputs(strand_one, strand_two)
