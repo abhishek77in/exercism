72ffba09f93c1eb993055ef86f8739f34775a028
Abhishek Srivastava <abhishek77in@gmail.com>
2016-01-11 15:17:59 +0530

    Solved hamming problem

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,6 +1,11 @@
 class Hamming
+  VERSION = 1
+
   def self.compute(strand_one, strand_two)
-    0
+    raise ArgumentError if strand_one.length != strand_two.length
+    (0...strand_one.length).to_a.collect { |index|
+      (strand_one[index] <=> strand_two[index]).abs
+    }.inject(:+).to_i
   end
 
 end
