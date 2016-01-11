c0f3d997bfa321ee7f12ddb015c3189e568a083f
Abhishek Srivastava <abhishek77in@gmail.com>
2016-01-11 19:53:04 +0530

    If which has return does not need to have else condition

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -5,13 +5,14 @@ var Year = function (input) {
 Year.prototype.isLeap = function(){
   if ( this.input % 400 == 0 ) {
     return true;
-  } else if ( this.input % 100 == 0 ) {
-    return false;
-  } else if ( this.input % 4 == 0 ) {
-    return true;
-  } else {
+  }
+  if ( this.input % 100 == 0 ) {
     return false;
   }
+  if ( this.input % 4 == 0 ) {
+    return true;
+  }
+  return false;
 };
 
 module.exports = Year;
